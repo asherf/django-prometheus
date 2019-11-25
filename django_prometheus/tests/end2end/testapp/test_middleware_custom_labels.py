57fbbf068e876feae3840e67dc1f4676695707de
Asher Foa <1268088+asherf@users.noreply.github.com>
2019-11-24 16:17:27 -0800

    debug

--- django_prometheus/tests/end2end/testapp/test_middleware_custom_labels.py
+++ django_prometheus/tests/end2end/testapp/test_middleware_custom_labels.py
@@ -37,6 +37,9 @@ class AppMetricsAfterMiddleware(PrometheusAfterMiddleware):
         if metric._name in EXTENDED_METRICS:
             new_labels = {"view_type": "foo", "user_agent_type": "browser"}
             new_labels.update(labels)
+            print(f"ASHER: Add labels {new_labels} - {metric._name}")
+        else:
+            print(f"ASHER: same labels {metric._name} == {labels}")
         return super(AppMetricsAfterMiddleware, self).label_metric(
             metric, request, response=response, **new_labels
         )
