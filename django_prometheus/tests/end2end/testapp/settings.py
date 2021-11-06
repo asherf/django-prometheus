daf63ab271a60fb84750a24201757e02e2b2f76c
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-11-06 13:52:13 -0700

    debug logging

--- django_prometheus/tests/end2end/testapp/settings.py
+++ django_prometheus/tests/end2end/testapp/settings.py
@@ -142,6 +142,6 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {"console": {"class": "logging.StreamHandler"}},
-    "root": {"handlers": ["console"], "level": "INFO"},
-    "loggers": {"django": {"handlers": ["console"], "level": "INFO"}},
+    "root": {"handlers": ["console"], "level": "DEBUG"},
+    "loggers": {"django": {"handlers": ["console"], "level": "DEBUG"}},
 }
